7699c61f29dc894ad74fe2888de84d91e62edbb9
Tom Anderson <tom.anderson@timgroup.com>
2012-05-16 15:58:41 +0100

    Recognise data lines as distinct from non-data lines

--- src/main/java/org/timgroup/dojo/WeatherMunging.java
+++ src/main/java/org/timgroup/dojo/WeatherMunging.java
@@ -1,15 +1,28 @@
 package org.timgroup.dojo;
 
+import java.util.regex.Pattern;
+
+import fj.F;
 import fj.data.Stream;
 
 public class WeatherMunging {
     private final Stream<String> lines;
+    
+    public static final F<String, Boolean> isDataLine = new F<String, Boolean>() {
+        
+        @Override
+        public Boolean f(String a) {
+            return Pattern.matches("\\s+[0-9]+.*", a);
+        }
+    };
 
     public WeatherMunging(Stream<String> lines) {
         this.lines = lines;
     }
 
     public int dayWithTheSmallestTemperatureSpread() {
+        final Stream<String> dataLines = lines.filter(isDataLine);
         return 0;
     }
+
 }
