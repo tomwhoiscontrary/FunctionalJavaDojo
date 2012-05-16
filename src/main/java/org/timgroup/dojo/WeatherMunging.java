1fb29d17a849e68246452c70a02f8b9a195d7814
Tom Anderson <tom.anderson@timgroup.com>
2012-05-16 16:12:45 +0100

    Map data lines to Day objects.

--- src/main/java/org/timgroup/dojo/WeatherMunging.java
+++ src/main/java/org/timgroup/dojo/WeatherMunging.java
@@ -5,8 +5,31 @@ import java.util.regex.Pattern;
 import fj.F;
 import fj.data.Stream;
 
+import static java.lang.Integer.parseInt;
+
 public class WeatherMunging {
     private final Stream<String> lines;
+    
+    public static class Day {
+
+        public int number;
+        public int maxTemp;
+        public int minTemp;
+        public Day(int number, int maxTemp, int minTemp) {
+            this.number = number;
+            this.maxTemp = maxTemp;
+            this.minTemp = minTemp;
+        }
+    };
+    
+    public static final F<String, Day> toDay = new F<String, WeatherMunging.Day>() {
+
+        @Override
+        public Day f(String a) {
+            final String[] cells = a.split("[^\\d]+");
+            return new Day(parseInt(cells[1]), parseInt(cells[2]), parseInt(cells[3]));
+        }
+    };
     
     public static final F<String, Boolean> isDataLine = new F<String, Boolean>() {
         
@@ -22,6 +45,7 @@ public class WeatherMunging {
 
     public int dayWithTheSmallestTemperatureSpread() {
         final Stream<String> dataLines = lines.filter(isDataLine);
+        Stream<Day> days = dataLines.map(toDay);
         return 0;
     }
 
--- src/test/java/org/timgroup/dojo/WeatherMungingTest.java
+++ src/test/java/org/timgroup/dojo/WeatherMungingTest.java
@@ -1,13 +1,44 @@
 package org.timgroup.dojo;
 
-import fj.data.Stream;
 import org.junit.Test;
+import org.timgroup.dojo.WeatherMunging.Day;
+
+import fj.data.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.timgroup.dojo.Files.readLinesFrom;
 
 public class WeatherMungingTest {
+    
+    @Test public void
+    recognises_data_lines() {
+        assertFalse(WeatherMunging.isDataLine.f(""));
+        assertFalse(WeatherMunging.isDataLine.f("(Unofficial, Preliminary Data). Source: <a"));
+        assertFalse(WeatherMunging.isDataLine.f("  mo  82.9  60.5  71.7    16  58.8       0.00              6.9          5.3"));
+        
+        assertTrue(WeatherMunging.isDataLine.f("   1  88    59    74          53.8       0.00 F       280  9.6 270  17  1.6  93 23 1004.5"));
+        assertTrue(WeatherMunging.isDataLine.f("   9  86    32*   59       6  61.5       0.00         240  7.6 220  12  6.0  78 46 1018.6"));
+    }
+    
+    @Test public void
+    parses_data_lines() {
+        Day day = WeatherMunging.toDay.f("   1  88    59    74          53.8       0.00 F       280  9.6 270  17  1.6  93 23 1004.5");
+        assertEquals(1, day.number);
+        assertEquals(88, day.maxTemp);
+        assertEquals(59, day.minTemp);
+    }
+
+    @Test public void
+    parses_data_lines_with_asterisks() {
+        Day day = WeatherMunging.toDay.f("   9  86    32*   59       6  61.5       0.00         240  7.6 220  12  6.0  78 46 1018.6");
+        assertEquals(9, day.number);
+        assertEquals(86, day.maxTemp);
+        assertEquals(32, day.minTemp);
+    }
 
     @Test
     public void weatherIsFine() {
